7f6ecaeb9fb05828c605b2986ec71f6a2732c7b8
islamkosker <73235010+islamkosker@users.noreply.github.com>
2026-07-07 18:36:46 +0300

    refactor(bnr): The binary decode and encode functions have been moved to bnr.h/bnr.c.

--- include/a429/bnr.h
+++ include/a429/bnr.h
@@ -11,8 +11,18 @@
  * @param error_code    The error code
  * @return double       The decoded real-world value (e.g., altitude, speed).
  */
-
 double a429_decode_bnr(uint32_t word, uint8_t bit_count, double scale_factor,
                        int8_t *error_code);
 
+/**
+ * @brief Encodes a double value into an ARINC 429 BNR word format.
+ * @param word Pointer to the 32-bit ARINC word where data will be set.
+ * @param value The real-world value to encode (e.g., 268.0).
+ * @param bit_count Number of bits used for the BNR data (including sign).
+ * Max 19.
+ * @param scale_factor The scale factor for this parameter.
+ * @param error_code Pointer to store the execution status.
+ */
+void a429_encode_bnr(uint32_t *word, double value, uint8_t bit_count,
+                     double scale_factor, int8_t *error_code);
 #endif
